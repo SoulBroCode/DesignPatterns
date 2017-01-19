d15ea3664d3e04fddeae4222236e1dee39fbdb41
SoulBroCode <linhhuynh2000@gmail.com>
2017-01-19 09:33:40 +0000

    forgot to push

--- Patterns/Character3.h
+++ Patterns/Character3.h
@@ -2,6 +2,5 @@
 class Character3
 {
 public:
-	virtual ~Character3() = 0;
 	virtual void Draw() = 0;
 };
--- Patterns/DrawAPI.h
+++ Patterns/DrawAPI.h
@@ -2,7 +2,6 @@
 class DrawAPI
 {
 public:
-	virtual ~DrawAPI() = 0;
 	virtual void Draw() = 0;
 };
 
